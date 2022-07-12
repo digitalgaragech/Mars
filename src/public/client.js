12be68c87ce9d7da08ac60ec154390b300747f5b
Didier.Grand <didier.grand@scott-sports.com>
2022-07-12 16:38:20 +0200

    In progress but not nice at all

--- src/public/client.js
+++ src/public/client.js
@@ -4,17 +4,18 @@ let store = {
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
 }
 
-
 // add our markup to the page
 const root = document.getElementById('root')
 
 const updateStore = (store, newState, roverName) => {
     store = Object.assign(store, newState)
-    render(root, store,roverName)
+    render(root, store, roverName)
 }
 
 const render = async (root, state, selectedRover) => {
-    root.innerHTML = App(state,selectedRover)
+
+
+    root.innerHTML = App(state,selectedRover) // Add main html structure with tabs
     
     document.getElementById("roverTabs").addEventListener("click", function(e) {
         if(e.target && e.target.nodeName == "LI") {
@@ -23,66 +24,106 @@ const render = async (root, state, selectedRover) => {
     });
 
     document.getElementById("imgList").addEventListener("click", function(e) {
-        if(e.target && e.target.nodeName == "IMG") {
+        if(e.target && e.target.getImageOfTheDaynodeName == "IMG") {
             showThisPic(e.target.src);
         }
     });
 }
 
-// create content
+// create structure
 const App = (state,selectedRover) => {
     let { rovers, apod } = state
+
+
+    // var ele = [];
+    // for (var i = 0; i < rovers.length; ++i) {
+    //     ele[i] = imgList(apod,rovers[i]);
+    //     console.log(ele[i]);
+    //     console.log(rovers[i]);
+    // }
+
     return `
     <header>Mars Dashboard</header>
         <main>
             <ul id="roverTabs">
                 ${Tabs(rovers)}
+                
             </ul>
-            <section>
-                ${roverInfos(apod,selectedRover)}
-            </section>
-            <section>
-                <img id="expandedImg" />
-                <ul id="imgList">
-                ${imgList(apod,selectedRover)}
-                </ul>
+            <section id="tabsContent">
+                ${TabContent(state,selectedRover)}
             </section>
         </main>
         <footer></footer>
     `
 }
+// create Tab Content
+const TabContent = (state,selectedRover) => {
+    let { apod } = state
+    return `
+        ${roverInfos(apod,selectedRover)}
+        <img id="expandedImg" />
+        <ul id="imgList">
+        ${imgList(apod,selectedRover)}
+        </ul>
+    `
+}
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-     render(root, store,"Curiosity");
+     
+    render(root, store, "Curiosity");
+
+
+
+
 })
 
 // ------------------------------------------------------  COMPONENTS
-
 // Create a list of rovers
 const Tabs = (tab) => {
     if (tab) {
-         // console.log(tab); [ "Curiosity", "Opportunity", "Spirit" ]
-        const roverTabs = `
-            ${tab.map(t => (`<li id="${t}">${t}</li>`)).join('')}
-        `
+        
+        getListOfRovers((data) => {
+             data.rovers.forEach(el => {
+                tab.push(el.name)
+             });
+        })
+
+        const roverList = Immutable.List(tab);
+        const roverList1 = roverList.toSet().toList();
+        const roverTabs = `${roverList1.map(t => (`<li id="${t}">${t}</li>`)).join('')}`
+       
         return roverTabs;
     }
 }
 
-// get the rover infos
-const roverInfos = (apod,rover) => {
-    if (!apod ) {
-        getImageOfTheDay(rover,store)
-    }
+
+
+const displayInfos = (rover) => {
+    console.log(rover);
+    console.log(rover.name);
+    console.log(rover.landing_date);
+    console.log(rover.launch_date);
+    console.log(rover.total_photos);
+
     let roverInfos = '';
-    roverInfos += `<h2>Name of the Rover : ${apod.image.photos[0].rover.name}</h2>`;
-    roverInfos += `<p>Landing date on Mars : ${apod.image.photos[0].rover.landing_date}</p>`;
-    roverInfos += `<p>Launch date from Earth : ${apod.image.photos[0].rover.launch_date}</p>`;
-    roverInfos += `<p>Status : ${apod.image.photos[0].rover.status}</p>`;
+    roverInfos += `<h2>Name of the Rover : ${rover.name}</h2>`;
+    roverInfos += `<p>Landing date on Mars : ${rover.landing_date}</p>`;
+    roverInfos += `<p>Launch date from Earth : ${rover.launch_date}</p>`;
+    roverInfos += `<p>Status : ${rover.status}</p>`;
     return (`
         ${roverInfos}
     `)
+}
+
+
+// get the rover infos
+const roverInfos = (rover) => {
+
+    getListOfRovers((data) => {
+        const found = data.rovers.find(el => el.name == rover)
+        displayInfos(found);
+    })
 }
 
 // Get the list of images
@@ -100,9 +141,9 @@ const imgList = (apod,rover) => {
     for(let n=0; n<nphotos; n++){
         imgFiles.push(apod.image.photos[n].img_src);
     }
-    const imgList = `${imgFiles.map(i => (`<li><img src="${i}" width="350" /></li>`)).join('')}`
+    // const imgList = `${imgFiles.map(i => (`<li><img src="${i}" width="350" /></li>`)).join('')}`
 
-    return imgList;
+    return imgFiles;
 }
 
 const showThisPic = (thisPic) => {
@@ -113,13 +154,23 @@ const showThisPic = (thisPic) => {
 
 // ------------------------------------------------------  API CALL
 
-// Example API call
-const getImageOfTheDay = (roverName,state) => {
-    let { apod } = state
+// API call
+const getImageOfTheDay = (roverName) => {
 
     fetch(`http://localhost:3000/apod/${roverName}`)
         .then(res => res.json())
         .then(apod => updateStore(store, { apod }, roverName))
+        .catch((error) => {
+            console.log(error)
+        });
 
 }
 
+const getListOfRovers = (callback) => {
+    fetch('http://localhost:3000/rovers')
+      .then(res => res.json())
+      .then(json => callback(json))
+      .catch((error) => {
+          console.log(error)
+      });
+  }
--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,17 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
 
-// example API call
+app.get('/rovers', async (req, res) => {
+    try {
+        const url = `https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${process.env.API_KEY}`
+        let rovers = await fetch(url)
+        rovers = await rovers.json();
+        res.send(rovers)
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
 app.get('/apod/:roverName', async (req, res) => {
     try {
         let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.roverName}/photos?sol=1000&api_key=${process.env.API_KEY}`)
